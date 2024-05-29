38861c0925787b82645176706f02a28a602fb656
xingang <findxingang@163.com>
2024-05-29 17:42:44 +0800

    feat: 基础版本代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +17,22 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    CommandLineRunner commandLineRunner(Environment environment) {
+        return args -> {
+            String appName = environment.getProperty("spring.application.name") != null ? environment.getProperty("spring.application.name") : "";
+            String port = environment.getProperty("server.port") != null ? environment.getProperty("server.port") : "8080";
+            String path = environment.getProperty("server.servlet.context-path") != null ? environment.getProperty("server.servlet.context-path") : "";
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(
+                    "\n\n\t" +
+                            "----------------------------------------------------------\n\t" +
+                            "Application " + appName +" is running! Access URLs:\n\t" +
+                            "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                            "External: \thttp://" + ip + ":" + port + path + "/\n\n\t" +
+                            "------------------------------------------------------------"
+            );
+        };
+    }
 }
