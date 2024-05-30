76be4353be44409b9913f37c2373521f4e332d43
xingang <findxingang@163.com>
2024-05-30 13:42:53 +0800

    feat: connect redis by lettuce

---
+++ demo-02-lettuce/src/main/java/com/example/lettuce/LettuceApplication.java
@@ -0,0 +1,41 @@
+package com.example.lettuce;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+
+/**
+ * @author xingang
+ * @since 2024/05/29 18:13
+ */
+@SpringBootApplication
+public class LettuceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LettuceApplication.class, args);
+    }
+
+    @Bean
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    CommandLineRunner commandLineRunner(Environment environment) {
+        return args -> {
+            String appName = environment.getProperty("spring.application.name") != null ? environment.getProperty("spring.application.name") : "";
+            String port = environment.getProperty("server.port") != null ? environment.getProperty("server.port") : "8080";
+            String path = environment.getProperty("server.servlet.context-path") != null ? environment.getProperty("server.servlet.context-path") : "";
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(
+                    "\n\n\t" +
+                            "----------------------------------------------------------\n\t" +
+                            "Application " + appName +" is running! Access URLs:\n\t" +
+                            "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                            "External: \thttp://" + ip + ":" + port + path + "/\n\n\t" +
+                            "------------------------------------------------------------"
+            );
+        };
+    }
+}
