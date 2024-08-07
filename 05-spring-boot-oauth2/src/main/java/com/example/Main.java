4a1aeda0ef42612f72dec54ca9623a16b3e299d5
wangxingang <wangxingang@crhms.cn>
2024-08-07 14:35:12 +0800

    feat(05-spring-boot-oauth2): 使用 Github 完成登录认证

---
+++ 05-spring-boot-oauth2/src/main/java/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+
+/**
+ * @author wangxingang
+ * @since 2024/8/7
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
