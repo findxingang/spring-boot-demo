d8bc217ba2aef1d97319a77b85658b176502411a
wangxingang <wangxingang@crhms.cn>
2024-08-16 13:03:19 +0800

    重构模块目录

---
+++ 03-Authentication-Authorization/03-spring-security-demo/src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootDemoApplication.class, args);
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        String appName = environment.getProperty("spring.application.name") != null ? environment.getProperty("spring.application.name") : "";
+        String port = environment.getProperty("server.port") != null ? environment.getProperty("server.port") : "8080";
+        String path = environment.getProperty("server.servlet.context-path") != null ? environment.getProperty("server.servlet.context-path") : "";
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        System.out.println(
+                "\n\n\t" +
+                        "----------------------------------------------------------\n\t" +
+                        "Application " + appName +" is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                        "External: \thttp://" + ip + ":" + port + path + "/\n\n\t" +
+                        "------------------------------------------------------------"
+        );
+    }
+
+}
